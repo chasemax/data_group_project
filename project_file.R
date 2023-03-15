a1c469c2b138b02d665c7236269eddd1d1b7cdb6
Chase Maxfield <maxfield.chase@gmail.com>
2023-03-15 14:06:31 -0600

    Initial Wrangling

---
+++ project_file.R
@@ -0,0 +1,25 @@
+library(tidyverse)
+
+team_results <- read_csv("data_group_project/data/MRegularSeasonDetailedResults.csv")
+
+team_results %>%
+  mutate(GameID = row_number()) %>%
+  select(-WLoc, -DayNum, -NumOT) %>%
+  pivot_longer(
+    cols = c(WTeamID, LTeamID),
+    names_to = "WinTeam",
+    values_to = "TeamID"
+  ) %>%
+  mutate(WinTeam = if_else(WinTeam == "WTeamID", 'Y', 'N')) %>%
+  pivot_longer(
+    cols = c(WScore, LScore, WFGM:LPF),
+    names_to = "Stat",
+    values_to = "StatValue"
+  ) %>%
+  filter((WinTeam == 'Y' & str_sub(Stat, end=1) == 'W') | (WinTeam == 'N' & str_sub(Stat, end=1) == 'L')) %>%
+  mutate(Stat = str_sub(Stat, 2)) %>%
+  select(-WinTeam) %>%
+  pivot_wider(
+    names_from = Stat,
+    values_from = StatValue
+  )
